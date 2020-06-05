ead6e0e83e0d9c7067924d0abbbb533a10b38e2f
luiz eduardo <l.eduardo8316@gmail.com>
2020-06-05 08:05:55 -0300

    finally mobile

--- server/src/controllers/ItemsController.ts
+++ server/src/controllers/ItemsController.ts
@@ -9,7 +9,7 @@ class ItemsController {
       return {
         id: item.id,
         title: item.title,
-        image_url: `http://localhost:3333/uploads/${item.image}`
+        image_url: `http://10.22.253.241:3333/uploads/${item.image}`
       }
     });
     return res.json(serializedItems);
